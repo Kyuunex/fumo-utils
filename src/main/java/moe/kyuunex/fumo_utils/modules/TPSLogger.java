ce17e20352e0449afa94ee994dca244236e788a9
Kyuunex <kyuunex@protonmail.ch>
2025-02-18 20:51:57 +0400

    add TPSLogger

---
+++ src/main/java/moe/kyuunex/fumo_utils/modules/TPSLogger.java
@@ -0,0 +1,37 @@
+package moe.kyuunex.fumo_utils.modules;
+
+import meteordevelopment.meteorclient.events.world.TickEvent;
+import meteordevelopment.meteorclient.settings.IntSetting;
+import meteordevelopment.meteorclient.settings.Setting;
+import meteordevelopment.meteorclient.settings.SettingGroup;
+import meteordevelopment.meteorclient.utils.world.TickRate;
+import moe.kyuunex.fumo_utils.FumoUtils;
+import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.orbit.EventHandler;
+
+public class TPSLogger extends Module {
+    private final SettingGroup sgDefault = settings.getDefaultGroup();
+    int tickTrack = 0;
+
+    private final Setting<Integer> interval = sgDefault.add(new IntSetting.Builder()
+        .name("interval")
+        .description("Interval in client side ticks")
+        .defaultValue(20)
+        .build()
+    );
+
+    public TPSLogger() {
+        super(FumoUtils.CATEGORY, "tps-logger", "Logs TPS in chat.");
+    }
+
+    @EventHandler
+    private void onTick(TickEvent.Post event) {
+        if(tickTrack >= interval.get()){
+            info(String.valueOf(TickRate.INSTANCE.getTickRate()));
+            tickTrack = 0;
+            return;
+        }
+        tickTrack++;
+    }
+}
+
